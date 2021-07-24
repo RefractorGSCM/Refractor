ca45c32fc83a7973220b5b2b43161169803f9518
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-07-24 12:26:40 -0400

    Authorizer GetPermissions method

--- domain/auth.go
+++ domain/auth.go
@@ -64,4 +64,5 @@ type AuthChecker func(permissions *bitperms.Permissions) (bool, error)
 // Authorizer represents an entity which can be used to determine if a user has access to perform an action or not.
 type Authorizer interface {
 	HasPermission(ctx context.Context, scope AuthScope, userID string, authChecker AuthChecker) (bool, error)
+	GetPermissions(ctx context.Context, scope AuthScope, userID string) (*bitperms.Permissions, error)
 }
