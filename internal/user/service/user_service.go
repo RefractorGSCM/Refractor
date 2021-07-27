880f7ac1dc5828eaa51e0fadbe1821389c2bdeaa
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-07-27 16:11:47 -0400

    UserService GetByID method

--- internal/user/service/user_service.go
+++ internal/user/service/user_service.go
@@ -58,44 +58,72 @@ func (s *userService) GetAllUsers(c context.Context) ([]*domain.User, error) {
 	var users []*domain.User
 
 	for _, au := range authUsers {
-		newUser := &domain.User{
-			ID:       au.Identity.Id,
-			Username: au.Traits.Username,
-		}
-
-		// Use the authorizer to get user the user's computed permissions, scoped to Refractor.
-		scope := domain.AuthScope{Type: domain.AuthObjRefractor}
-		permissions, err := s.authorizer.GetPermissions(ctx, scope, newUser.ID)
+		newUser, err := s.getUserInfo(ctx, au)
 		if err != nil {
-			s.logger.Error("Could not get computed permissions for user", zap.String("userID", newUser.ID), zap.Error(err))
-			return nil, errors.Wrap(err, fmt.Sprintf("user ID: %s", newUser.ID))
+			return nil, err
 		}
-
-		newUser.Permissions = permissions.String()
-
-		// Use the groups repo to get the user's groups
-		groups, err := s.groupRepo.GetUserGroups(ctx, newUser.ID)
-		if errors.Cause(err) == domain.ErrNotFound {
-			groups = []*domain.Group{}
-		} else if err != nil {
-			s.logger.Error("Could not get groups for user", zap.String("userID", newUser.ID), zap.Error(err))
-			return nil, errors.Wrap(err, fmt.Sprintf("user ID: %s", newUser.ID))
-		}
-
-		newUser.Groups = groups
-
-		// Get user meta
-		meta, err := s.metaRepo.GetByID(ctx, newUser.ID)
-		if err != nil {
-			s.logger.Error("Could not get meta for user", zap.String("userID", newUser.ID), zap.Error(err))
-			return nil, errors.Wrap(err, fmt.Sprintf("user ID: %s", newUser.ID))
-		}
-
-		newUser.UserMeta = meta
 
 		// Add user to list
 		users = append(users, newUser)
 	}
 
 	return users, nil
+}
+
+func (s *userService) GetByID(c context.Context, userID string) (*domain.User, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+
+	au, err := s.authRepo.GetUserByID(ctx, userID)
+	if err != nil {
+		s.logger.Error("Could not GetUserByID from auth repo", zap.String("UserID", userID), zap.Error(err))
+		return nil, err
+	}
+
+	user, err := s.getUserInfo(ctx, au)
+	if err != nil {
+		s.logger.Error("Could not get user info", zap.String("UserID", userID), zap.Error(err))
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *userService) getUserInfo(ctx context.Context, authUser *domain.AuthUser) (*domain.User, error) {
+	newUser := &domain.User{
+		ID:       authUser.Identity.Id,
+		Username: authUser.Traits.Username,
+	}
+
+	// Use the authorizer to get user the user's computed permissions, scoped to Refractor.
+	scope := domain.AuthScope{Type: domain.AuthObjRefractor}
+	permissions, err := s.authorizer.GetPermissions(ctx, scope, newUser.ID)
+	if err != nil {
+		s.logger.Error("Could not get computed permissions for user", zap.String("userID", newUser.ID), zap.Error(err))
+		return nil, errors.Wrap(err, fmt.Sprintf("user ID: %s", newUser.ID))
+	}
+
+	newUser.Permissions = permissions.String()
+
+	// Use the groups repo to get the user's groups
+	groups, err := s.groupRepo.GetUserGroups(ctx, newUser.ID)
+	if errors.Cause(err) == domain.ErrNotFound {
+		groups = []*domain.Group{}
+	} else if err != nil {
+		s.logger.Error("Could not get groups for user", zap.String("userID", newUser.ID), zap.Error(err))
+		return nil, errors.Wrap(err, fmt.Sprintf("user ID: %s", newUser.ID))
+	}
+
+	newUser.Groups = groups
+
+	// Get user meta
+	meta, err := s.metaRepo.GetByID(ctx, authUser.Identity.Id)
+	if err != nil {
+		s.logger.Error("Could not get meta for user", zap.String("userID", newUser.ID), zap.Error(err))
+		return nil, errors.Wrap(err, fmt.Sprintf("user ID: %s", newUser.ID))
+	}
+
+	newUser.UserMeta = meta
+
+	return newUser, nil
 }
