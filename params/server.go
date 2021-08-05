4ba4283e719613f002be06ffb33f55a0ace75dee
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-08-05 18:44:01 -0400

    CreateServerParams validation params

---
+++ params/server.go
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package params
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"strings"
+)
+
+type CreateServerParams struct {
+	Game         string `form:"game" json:"game"`
+	Name         string `form:"name" json:"name"`
+	Address      string `form:"address" json:"address"`
+	RCONPort     string `form:"rcon_port" json:"rcon_port"`
+	RCONPassword string `form:"rcon_password" json:"rcon_password"`
+}
+
+func (body CreateServerParams) Validate() error {
+	body.Game = strings.TrimSpace(body.Game)
+	body.Name = strings.TrimSpace(body.Name)
+	body.Address = strings.TrimSpace(body.Address)
+	body.RCONPort = strings.TrimSpace(body.RCONPort)
+	body.RCONPassword = strings.TrimSpace(body.RCONPassword)
+
+	return ValidateStruct(&body,
+		validation.Field(&body.Game, validation.Required, validation.Length(1, 32)),
+		validation.Field(&body.Name, validation.Required, validation.Length(1, 20)),
+		validation.Field(&body.Address, validation.Required, is.IPv4),
+		validation.Field(&body.RCONPort, validation.Required, is.Port),
+		validation.Field(&body.RCONPassword, validation.Required, validation.Length(1, 128)),
+	)
+}
