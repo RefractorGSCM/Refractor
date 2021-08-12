1587f3abec18c6e5c64cc5fcab9379be337aa10c
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-08-12 16:28:39 -0400

    Websocket types

---
+++ domain/websocket.go
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package domain
+
+import "net"
+
+type WebsocketMessage struct {
+	Type string      `json:"type"`
+	Body interface{} `json:"body"`
+}
+
+type WebsocketDirectMessage struct {
+	ClientID int64
+	Message  *WebsocketMessage
+}
+
+type WebsocketService interface {
+	CreateClient(userID string, conn net.Conn)
+	StartPool()
+	Broadcast(message *WebsocketMessage)
+}
