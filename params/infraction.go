1b490ce699806865b20697620e4f7cc9336576bc
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-08-31 15:30:42 -0400

    Infraction CreateWarningParams

---
+++ params/infraction.go
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package params
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"math"
+	"strings"
+)
+
+type CreateWarningParams struct {
+	PlayerID int64  `json:"player_id" form:"player_id"`
+	Platform string `json:"platform" form:"platform"`
+	ServerID int64  `json:"server_id" form:"server_id"`
+	Reason   string `json:"reason" form:"reason"`
+}
+
+func (body CreateWarningParams) Validate() error {
+	body.Reason = strings.TrimSpace(body.Reason)
+
+	return ValidateStruct(&body,
+		validation.Field(&body.PlayerID, validation.Required, validation.Length(1, 80)),
+		validation.Field(&body.Platform, validation.Required, validation.Length(1, 128)),
+		validation.Field(&body.ServerID, validation.Required, is.Int, validation.Min(1), validation.Max(math.MaxInt32)),
+		validation.Field(&body.Reason, validation.Required, validation.Length(1, 1024)))
+}
