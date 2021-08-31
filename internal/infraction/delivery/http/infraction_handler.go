7e4b9510057036e75e9f36b79f519df2d7ced74a
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-08-31 17:03:40 -0400

    Infraction handler Mute, Kick and Ban creation

--- internal/infraction/delivery/http/infraction_handler.go
+++ internal/infraction/delivery/http/infraction_handler.go
@@ -52,6 +52,9 @@ func ApplyInfractionHandler(apiGroup *echo.Group, s domain.InfractionService, a 
 	//}, log)
 
 	infractionGroup.POST("/warning/:serverId", handler.CreateWarning)
+	infractionGroup.POST("/mute/:serverId", handler.CreateMute)
+	infractionGroup.POST("/kick/:serverId", handler.CreateKick)
+	infractionGroup.POST("/ban/:serverId", handler.CreateBan)
 }
 
 func (h *infractionHandler) CreateWarning(c echo.Context) error {
@@ -100,5 +103,152 @@ func (h *infractionHandler) CreateWarning(c echo.Context) error {
 		Success: true,
 		Message: "Warning created",
 		Payload: newWarning,
+	})
+}
+
+func (h *infractionHandler) CreateMute(c echo.Context) error {
+	serverIDString := c.Param("serverId")
+
+	serverID, err := strconv.ParseInt(serverIDString, 10, 64)
+	if err != nil {
+		return domain.NewHTTPError(fmt.Errorf("invalid server id"), http.StatusBadRequest, "")
+	}
+
+	// Validate request body
+	var body params.CreateMuteParams
+	if err := c.Bind(&body); err != nil {
+		return err
+	}
+
+	if ok, err := api.ValidateRequestBody(body); !ok {
+		return err
+	}
+
+	user, ok := c.Get("user").(*domain.AuthUser)
+	if !ok {
+		return fmt.Errorf("could not cast user to *domain.AuthUser")
+	}
+
+	// Create the new warning
+	newMute := &domain.Infraction{
+		PlayerID:     body.PlayerID,
+		Platform:     body.Platform,
+		UserID:       null.NewString(user.Identity.Id, true),
+		ServerID:     serverID,
+		Type:         domain.InfractionTypeMute,
+		Reason:       null.NewString(body.Reason, true),
+		Duration:     null.NewInt(int64(body.Duration), true),
+		SystemAction: false,
+		CreatedAt:    null.Time{},
+		ModifiedAt:   null.Time{},
+	}
+
+	newMute, err = h.service.Store(c.Request().Context(), newMute)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, &domain.Response{
+		Success: true,
+		Message: "Mute created",
+		Payload: newMute,
+	})
+}
+
+func (h *infractionHandler) CreateKick(c echo.Context) error {
+	serverIDString := c.Param("serverId")
+
+	serverID, err := strconv.ParseInt(serverIDString, 10, 64)
+	if err != nil {
+		return domain.NewHTTPError(fmt.Errorf("invalid server id"), http.StatusBadRequest, "")
+	}
+
+	// Validate request body
+	var body params.CreateKickParams
+	if err := c.Bind(&body); err != nil {
+		return err
+	}
+
+	if ok, err := api.ValidateRequestBody(body); !ok {
+		return err
+	}
+
+	user, ok := c.Get("user").(*domain.AuthUser)
+	if !ok {
+		return fmt.Errorf("could not cast user to *domain.AuthUser")
+	}
+
+	// Create the new warning
+	newKick := &domain.Infraction{
+		PlayerID:     body.PlayerID,
+		Platform:     body.Platform,
+		UserID:       null.NewString(user.Identity.Id, true),
+		ServerID:     serverID,
+		Type:         domain.InfractionTypeKick,
+		Reason:       null.NewString(body.Reason, true),
+		Duration:     null.Int{},
+		SystemAction: false,
+		CreatedAt:    null.Time{},
+		ModifiedAt:   null.Time{},
+	}
+
+	newKick, err = h.service.Store(c.Request().Context(), newKick)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, &domain.Response{
+		Success: true,
+		Message: "Kick created",
+		Payload: newKick,
+	})
+}
+
+func (h *infractionHandler) CreateBan(c echo.Context) error {
+	serverIDString := c.Param("serverId")
+
+	serverID, err := strconv.ParseInt(serverIDString, 10, 64)
+	if err != nil {
+		return domain.NewHTTPError(fmt.Errorf("invalid server id"), http.StatusBadRequest, "")
+	}
+
+	// Validate request body
+	var body params.CreateBanParams
+	if err := c.Bind(&body); err != nil {
+		return err
+	}
+
+	if ok, err := api.ValidateRequestBody(body); !ok {
+		return err
+	}
+
+	user, ok := c.Get("user").(*domain.AuthUser)
+	if !ok {
+		return fmt.Errorf("could not cast user to *domain.AuthUser")
+	}
+
+	// Create the new warning
+	newBan := &domain.Infraction{
+		PlayerID:     body.PlayerID,
+		Platform:     body.Platform,
+		UserID:       null.NewString(user.Identity.Id, true),
+		ServerID:     serverID,
+		Type:         domain.InfractionTypeBan,
+		Reason:       null.NewString(body.Reason, true),
+		Duration:     null.NewInt(int64(body.Duration), true),
+		SystemAction: false,
+		CreatedAt:    null.Time{},
+		ModifiedAt:   null.Time{},
+	}
+
+	newBan, err = h.service.Store(c.Request().Context(), newBan)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, &domain.Response{
+		Success: true,
+		Message: "Ban created",
+		Payload: newBan,
 	})
 }
