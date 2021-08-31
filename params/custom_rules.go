55b07c76fcdf20c2c5eeb356dfc51ab3b6a01337
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-08-31 16:10:47 -0400

    valueInStrArray param validation function

---
+++ params/custom_rules.go
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package params
+
+import (
+	"fmt"
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/pkg/errors"
+)
+
+func valueInStrArray(arr []string) validation.RuleFunc {
+	return func(value interface{}) error {
+		value, _ = value.(string)
+
+		for _, val := range arr {
+			if val == value {
+				return nil
+			}
+		}
+
+		return errors.New(fmt.Sprintf("must be one of: %v", arr))
+	}
+}
