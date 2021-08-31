f12470239b740474e782f34351653a7ae9ce5345
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-08-30 20:35:30 -0400

    Infractions types and Postgres migrations

---
+++ migrations/20210830202202_create_infractions_table.up.sql
@@ -0,0 +1,40 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+DROP TYPE IF EXISTS InfractionType;
+CREATE TYPE InfractionType AS ENUM ('WARNING', 'MUTE', 'KICK', 'BAN');
+
+CREATE TABLE IF NOT EXISTS Infractions(
+    InfractionID SERIAL NOT NULL PRIMARY KEY,
+    PlayerID VARCHAR(80) NOT NULL,
+    Platform VARCHAR(128) NOT NULL,
+    UserID VARCHAR(36),
+    ServerID SERIAL NOT NULL,
+    Type InfractionType NOT NULL,
+    Reason TEXT NOT NULL DEFAULT '',
+    Duration INT,
+    SystemAction BOOLEAN DEFAULT FALSE,
+    CreatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ModifiedAt TIMESTAMP,
+
+    FOREIGN KEY (PlayerID, Platform) REFERENCES Players (PlayerID, Platform),
+    FOREIGN KEY (ServerID) References Servers (ServerID)
+);
+
+DROP TRIGGER IF EXISTS update_infractions_modat ON Infractions;
+CREATE TRIGGER update_infractions_modat BEFORE UPDATE ON Infractions
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_at_column();
