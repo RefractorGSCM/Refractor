17015fa4d758d637ba15615277405662db9c2ae4
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-09-26 16:12:20 -0400

    Chat domain types

---
+++ domain/chat.go
@@ -0,0 +1,50 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package domain
+
+import (
+	"github.com/guregu/null"
+	"time"
+)
+
+type ChatReceiveBody struct {
+	ServerID   int64  `json:"server_id"`
+	PlayerID   string `json:"player_id"`
+	Platform   string `json:"platform"`
+	Name       string `json:"name"`
+	Message    string `json:"message"`
+	SentByUser bool   `json:"sent_by_user"`
+}
+
+type ChatMessage struct {
+	MessageID  int64     `json:"id"`
+	PlayerID   string    `json:"player_id"`
+	Platform   string    `json:"platform"`
+	ServerID   int64     `json:"server_id"`
+	Message    string    `json:"message"`
+	Flagged    bool      `json:"flagged"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt null.Time `json:"modified_at"`
+	PlayerName string    `json:"player_name,omitempty"` // not a db field, must be populated manually.
+}
+
+type ChatRepo interface {
+	Create(message *ChatMessage) error
+	FindByID(id int64) (*ChatMessage, error)
+	FindMany(args FindArgs) ([]*ChatMessage, error)
+}
