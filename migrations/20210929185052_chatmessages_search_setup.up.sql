aa11095198aaf2521d39f5418eb84daf71d76798
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-09-29 19:01:14 -0400

    ChatMessages search setup migrations
    Added a MessageVectors column to ChatMessages to pre-compute search vectors of ChatMessages to speed up search.
    
    Created a function to update chat message vectors.
    
    Created a trigger to automatically call the update message vector function when the message column is updated to automatically re-compute vectors.

---
+++ migrations/20210929185052_chatmessages_search_setup.up.sql
@@ -0,0 +1,32 @@
+/*
+ * This file is part of Refractor.
+ *
+ * Refractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+alter table ChatMessages add MessageVectors tsvector;
+create index idx_search_message_vectors on ChatMessages using gin(MessageVectors);
+update ChatMessages set MessageVectors = to_tsvector(Message);
+
+create or replace function update_chatmessage_vectors()
+returns trigger as $$
+begin
+    new.MessageVectors = to_tsvector(new.Message);
+    return new;
+end;
+$$ language 'plpgsql';
+
+drop trigger if exists update_chatmessages_msgvecs on ChatMessages;
+create trigger update_chatmessages_msgvecs before update of Message on ChatMessages
+    for each row execute procedure update_chatmessage_vectors();
