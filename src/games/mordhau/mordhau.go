9e138a2c6482742cfd40e186e7c057de527109ea
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-11-15 18:54:49 -0500

    Reverted test Mordhau config changes

--- src/games/mordhau/mordhau.go
+++ src/games/mordhau/mordhau.go
@@ -39,8 +39,7 @@ func NewMordhauGame(platform domain.Platform) domain.Game {
 			AlivePingInterval: time.Second * 10,
 			EnableBroadcasts:  true,
 			RCONInitCommands:  []string{"listen login", "listen chat"},
-			//PlayerListRefreshInterval: time.Minute * 10,
-			PlayerListRefreshInterval: time.Second * 20,
+			PlayerListRefreshInterval: time.Minute * 10,
 			EnableChat:                true,
 			BroadcastPatterns: map[string]*regexp.Regexp{
 				broadcast.TypeJoin: regexp.MustCompile("^Login: (?P<Date>[0-9\\.-]+): (?P<Name>.+) \\((?P<PlayerID>[0-9a-fA-F]+)\\) logged in$"),
